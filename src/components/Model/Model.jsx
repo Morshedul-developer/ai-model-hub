0ff995cb006cd566cc7f1da23f3e227471ccdbfa
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-29 19:46:24 +0600

    fixed a bug to the buttons

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,7 +1,7 @@
 const Model = ({ model }) => {
   const { title, description, price, image, status } = model;
   return (
-    <div className="border border-zinc-200 shadow-lg rounded-2xl overflow-hidden">
+    <div className="border border-zinc-200 shadow-lg rounded-2xl overflow-hidden flex flex-col">
       <div className="flex justify-center h-50 bg-zinc-300">
         <img
           className="w-35 object-contain rounded-2xl"
@@ -9,11 +9,13 @@ const Model = ({ model }) => {
           alt={title}
         />
       </div>
-      <div className="p-5 space-y-3 flex flex-col">
+      <div className="p-5 space-y-3 flex flex-col flex-1">
         <h3 className="font-bold text-2xl">{title}</h3>
         <p className="text-gray-600">{description}</p>
         <p className="font-bold text-xl">${price}/month</p>
-        <button className="btn w-full bg-red-500 text-white rounded-md hover:bg-red-600">Subscribe Now</button>
+        <button className="btn w-full bg-red-500 text-white rounded-md hover:bg-red-600 mt-auto">
+          Subscribe Now
+        </button>
       </div>
     </div>
   );
