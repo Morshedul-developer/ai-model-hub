181f74a4b8b21f6ba366c95437511274bf7074f1
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-29 21:57:59 +0600

    set a state to the card button and used the useState hook

--- src/components/Model/Model.jsx
+++ src/components/Model/Model.jsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+
 const Model = ({ model }) => {
-  const { title, description, price, image, status } = model;
+  const [isSubscribed, setIsSubscribed] = useState(false);
+  const handleSubscribe = () => {
+    setIsSubscribed(true);
+  };
+
+  const { title, description, price, image } = model;
   return (
     <div className="border border-zinc-200 shadow-lg rounded-2xl overflow-hidden flex flex-col">
       <div className="flex justify-center h-50 bg-zinc-300">
@@ -13,8 +20,11 @@ const Model = ({ model }) => {
         <h3 className="font-bold text-2xl">{title}</h3>
         <p className="text-gray-600">{description}</p>
         <p className="font-bold text-xl">${price}/month</p>
-        <button className="btn w-full bg-red-500 text-white rounded-md hover:bg-red-600 mt-auto">
-          Subscribe Now
+        <button
+          onClick={handleSubscribe}
+          className="btn w-full bg-red-500 text-white rounded-md hover:bg-red-600 mt-auto"
+        >
+          {isSubscribed ? "Subscribed" : "Subscribe Now"}
         </button>
       </div>
     </div>
