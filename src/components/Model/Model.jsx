7923a60c9106f40419bbdc8c72703e63fc5781ad
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-29 19:40:38 +0600

    fetched all the datas and made cards and designed them

---
+++ src/components/Model/Model.jsx
@@ -0,0 +1,22 @@
+const Model = ({ model }) => {
+  const { title, description, price, image, status } = model;
+  return (
+    <div className="border border-zinc-200 shadow-lg rounded-2xl overflow-hidden">
+      <div className="flex justify-center h-50 bg-zinc-300">
+        <img
+          className="w-35 object-contain rounded-2xl"
+          src={image}
+          alt={title}
+        />
+      </div>
+      <div className="p-5 space-y-3 flex flex-col">
+        <h3 className="font-bold text-2xl">{title}</h3>
+        <p className="text-gray-600">{description}</p>
+        <p className="font-bold text-xl">${price}/month</p>
+        <button className="btn w-full bg-red-500 text-white rounded-md hover:bg-red-600">Subscribe Now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Model;
--- src/components/Models/Models.jsx
+++ src/components/Models/Models.jsx
@@ -1,18 +1,19 @@
 import { use } from "react";
+import Model from "../Model/Model";
 
 const Models = ({ modelPromise }) => {
     const models = use(modelPromise);
     
   return (
-    <div className="py-20">
+    <div className="py-20 max-w-7xl mx-auto space-y-10">
       <div className="text-center space-y-2">
         <h2 className="font-bold text-4xl">Choose Your AI Model</h2>
         <p className="text-gray-600">Our subscription gives you access to all frontier AI models</p>
       </div>
 
-      <div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7">
         {
-            models.map(model => <h3 key={model.id} className="text-2xl font-semibold">{model.title}</h3>)
+            models.map(model => <Model key={model.id} model={model} />)
         }
       </div>
     </div>
