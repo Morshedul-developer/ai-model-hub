ee715a68eb720b19901fbd97ceb537d83db41303
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-04-29 17:43:08 +0600

    created the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello, React!
-      </h1>
-      <button className="btn">Click me</button>
+      <NavBar></NavBar>
     </>
   );
 }
